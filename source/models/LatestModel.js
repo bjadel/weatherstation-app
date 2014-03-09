441c02a026283a9f40d09863a0f6471bb77a058f
bjadel <bjawebos@adelberg-online.de>
2014-03-09 03:36:31 +0100

    add ilib and format/convert date

--- source/models/LatestModel.js
+++ source/models/LatestModel.js
@@ -3,5 +3,33 @@ enyo.kind({
 	kind: "enyo.Model",
 	getUrl: function() {
             	return "http://www.adelberg-online.de/weatherstation/ws/app/latest";
-    		}
+    		},
+    parse: function(data) {
+    	// format date
+    	var timestamp = data.date;
+		var dateUTC = new Date(timestamp);
+		var dateClient = new Date(Date.UTC(dateUTC.getFullYear(), dateUTC.getMonth(), dateUTC.getDate(), dateUTC.getHours(), dateUTC.getMinutes(), dateUTC.getSeconds()));		
+		
+		var cal = ilib.Cal.newInstance({
+            locale: ilib.getLocale(),
+            type: 'gregorian'
+        });
+        var date = cal.newDateInstance({
+            year: dateClient.getFullYear(),
+            month: dateClient.getMonth() + 1,
+            day: dateClient.getDate(),
+            hour: dateClient.getHours(),
+            minute: dateClient.getMinutes(),
+            second: dateClient.getSeconds(),
+            millisecond: 0
+        });
+
+		var options = {};
+        options['calendar'] = 'gregorian';
+        options['length'] = 'full';
+        options['type'] = 'datetime';
+		var fmt = new ilib.DateFmt(options);
+		data.date = fmt.format(date);
+    	return data;
+    }
 });
