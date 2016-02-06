c3b010e0bab163df32c0880ee35a167cd5717049
Björn Adelberg <bja@tud>
2016-02-06 09:29:54 +0100

    persist settings

--- source/models/LatestModel.js
+++ source/models/LatestModel.js
@@ -2,10 +2,8 @@ enyo.kind({
 	name: "LatestModel",
 	kind: "enyo.Collection",
 	getUrl: function() {
-		//var url = "http://www.adelberg-online.de/weatherstation/ws/app/location/"+this.get('locationId'(+"/latest";
-		var url = "http://localhost:8080/weatherstation/ws/app/location/"+this.get('locationId')+"/latest";
-
-    return url;
+		var url = "http://www.adelberg-online.de/weatherstation/ws/app/location/"+this.get('locationId')+"/latest";
+		return url;
   },
   parse: function(data) {
   	return data.result;
